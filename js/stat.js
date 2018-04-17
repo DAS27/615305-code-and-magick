d68b00ae4778a8bb6b7822ecb86596b6646e4988
Damir Sarkulin <sarkulin@inbox.ru>
2018-04-17 09:52:18 +0500

    FIX Облако статистики

--- js/stat.js
+++ js/stat.js
@@ -4,26 +4,29 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var GAP = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
+window.renderStatistics = function (ctx, names) {
+  renderCloud(ctx, CLOUD_X + CLOUD_Y, CLOUD_Y + CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16 px Mono PT';
+  ctx.textBaseline = 'hanging';
 
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, barWidth, BAR_HEIGHT);
-  }
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 70);
+
+
+
+  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  // ctx.fillText('Вы', 120, 100);
+  // ctx.fillRect(CLOUD_X + CLOUD_Y + CLOUD_Y, CLOUD_X, BAR_WIDTH, BAR_HEIGHT);
 };
